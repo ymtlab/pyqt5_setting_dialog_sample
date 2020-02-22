ea3f7f8b6a2bd154a36153741bdd8b5ea39e8272
ymtlab <60970172+ymtlab@users.noreply.github.com>
2020-02-22 10:46:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from mainwindow import Ui_MainWindow
+from setting_dialog import Ui_Dialog
+from PyQt5 import QtWidgets, QtCore
+
+class SettingDialog(QtWidgets.QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+    def plane_text(self):
+        result = self.exec_()
+        plane_text = self.ui.textEdit.toPlainText()
+        return (plane_text, result == QtWidgets.QDialog.Accepted)
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, app):
+        super().__init__()
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.actionSetting.triggered.connect(self.show_setting_dialog)
+
+    def show_setting_dialog(self):
+        plane_text, result = SettingDialog(self).plane_text()
+        if result:
+            self.ui.textBrowser.setPlainText(plane_text)
+ 
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow(app)
+    window.show()
+    app.exec_()
+ 
+if __name__ == '__main__':
+    main()
---
+++ mainwindow.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(300, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName("textBrowser")
+        self.verticalLayout.addWidget(self.textBrowser)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 300, 21))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionSetting = QtWidgets.QAction(MainWindow)
+        self.actionSetting.setObjectName("actionSetting")
+        self.menu.addAction(self.actionSetting)
+        self.menubar.addAction(self.menu.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu.setTitle(_translate("MainWindow", "Setting"))
+        self.actionSetting.setText(_translate("MainWindow", "Setting"))
